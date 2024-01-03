6561035766bb9f9422f14abfc73f79c1145adf13
DevGustavus <gustavusinvictus@gmail.com>
2024-01-03 06:38:03 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,41 @@
+const emojis = [
+    "☢️",
+    "☢️",
+    "🌎",
+    "🌎",
+    "🍉",
+    "🍉",
+    "🍔",
+    "🍔",
+    "🤑",
+    "🤑",
+    "☠️",
+    "☠️",
+    "🧙‍♂️",
+    "🧙‍♂️",
+    "💣",
+    "💣"
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
